77c227bb5f2df93967517937f84537dc922fc9e9
santiago hurtado <sahurtaro@gmail.com>
2025-06-03 14:55:50 -0500

    chore: add .env file and move environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,21 @@ import { AuthorsModule } from './authors/authors.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CommentsModule } from './comments/comments.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // TypeORM configuration for connecting to a MySQL database,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'user_books',
-      password: 'root',
-      database: 'db_books',
+      host: process.env.MY_SQL_HOST,
+      port: parseInt(process.env.MY_SQL_PORT ?? '3307'), // default to 3306 if undefined
+      username: process.env.MY_SQL_USERNAME,
+      password: process.env.MY_SQL_PASSWORD,
+      database: process.env.MY_SQL_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
     }),
